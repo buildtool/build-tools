127d9aa004bfb5d6f1095656cd4a5bbcef455e4a
Joakim Olsson <joakim@unbound.se>
2018-11-16 12:47:18 +0100

    Add source:ing of .buildtools-files in parent directories

--- commons.sh
+++ commons.sh
@@ -13,3 +13,12 @@ die() {
 try() {
   "$@" || die "cannot $*";
 }
+
+upfind() {
+  IFS=/; dn=($1); ct=${#dn[@]}
+  for((i=0; i<ct; i++)); do
+    subd+=/"${dn[i]}"
+    dots=$(for((j=ct-i; j>1; j--)); do printf "../"; done)
+    find "$subd" -maxdepth 1 -type f -name "$2" -printf "$dots%f\n"
+  done
+}
--- environment.sh
+++ environment.sh
@@ -2,12 +2,17 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${SCRIPT_DIR}/commons.sh
 
-declare -A valid_environments
-valid_environments=(
-["local"]="--context docker-for-desktop --namespace default"
-["staging"]="--context docker-for-desktop --namespace staging"
-["prod"]="--context docker-for-desktop --namespace prod"
-)
+# Add a version of the definition below to a .buildtools file somewhere from here and upwards in the directory structure
+#declare -A valid_environments
+#valid_environments=(
+#["local"]="--context docker-for-desktop --namespace default"
+#["staging"]="--context k8s.cluster --namespace staging"
+#["prod"]="--context k8s.cluster --namespace default"
+#)
+
+for CONFIG in $(upfind ${PWD} .buildtools); do
+  source ${CONFIG}
+done
 
 environment:check_args() {
   : ${1:?"Usage: $0 ENVIRONMENT"}
