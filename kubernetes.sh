617012374462b834abe1bae87e7f21041a3933da
Joakim Olsson <joakim@unbound.se>
2018-11-18 13:28:30 +0100

    Add creation of mysql-user and loading of data

--- kubernetes.sh
+++ kubernetes.sh
@@ -80,3 +80,29 @@ kubernetes:create_database_user() {
  --from-literal=USERNAME="${SERVICE_NAME}" \
  --from-literal=PASSWORD="${SERVICE_NAME}"
 }
+
+kubernetes:create_mysql_user() {
+  local ENVIRONMENT="${1}"
+  local SERVICE_NAME="${2}"
+  local KUBECTL_CMD=$(kubernetes:get_command ${ENVIRONMENT})
+  db_pod_name=$(${KUBECTL_CMD} get pods --selector 'app=mysql' --output jsonpath={.items..metadata.name})
+
+  ${KUBECTL_CMD} exec -it ${db_pod_name} -- bash -c "echo \"CREATE USER IF NOT EXISTS ${SERVICE_NAME} IDENTIFIED BY '${SERVICE_NAME}';CREATE DATABASE IF NOT EXISTS ${SERVICE_NAME}; GRANT ALL ON ${SERVICE_NAME}.* TO ${SERVICE_NAME};\" | mysql -u root -p\"password\""
+
+  local SECRET_NAME="${SERVICE_NAME}-db"
+  ${KUBECTL_CMD} delete secret ${SECRET_NAME} &> /dev/null || true
+  ${KUBECTL_CMD} create secret generic ${SECRET_NAME} \
+ --from-literal=USERNAME="${SERVICE_NAME}" \
+ --from-literal=PASSWORD="${SERVICE_NAME}"
+}
+
+kubernetes:load_mysql_data() {
+  local ENVIRONMENT="$1"
+  local SERVICE_NAME="$2"
+  local FILE="$3"
+
+  local KUBECTL_CMD=$(kubernetes:get_command ${ENVIRONMENT})
+  db_pod_name=$(${KUBECTL_CMD} get pods --selector 'app=mysql' --output jsonpath={.items..metadata.name})
+
+  cat "$FILE" | ${KUBECTL_CMD} exec -it ${db_pod_name} -- bash -c "mysql -u $SERVICE_NAME -p\"$SERVICE_NAME\" $SERVICE_NAME"
+}
