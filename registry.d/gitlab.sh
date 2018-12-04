ee025580f035d154bd2b78f3a28e4fc762b4ad9d
Joakim Olsson <joakim@unbound.se>
2018-12-04 15:14:08 +0100

    Set DOCKER_REGISTRY_URL to CI_REGISTRY_IMAGE with image-part stripped

--- registry.d/gitlab.sh
+++ registry.d/gitlab.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 if [ -n "${CI_REGISTRY_IMAGE:-}" ]; then
+  DOCKER_REGISTRY_URL=${CI_REGISTRY_IMAGE%/*}
+
   registry:login() {
     docker login -u gitlab-ci-token -p ${CI_BUILD_TOKEN} ${CI_REGISTRY}
   }
