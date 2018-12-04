c3d95e0548b207d4d89f4f21352c92e83e149af3
Joakim Olsson <joakim@unbound.se>
2018-12-04 20:52:29 +0100

    Set DOCKER_REGISTRY_URL based on DOCKERHUB_REPOSITORY

--- registry.d/dockerhub.sh
+++ registry.d/dockerhub.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-if [ -n "${DOCKERHUB_USERNAME:-}" ]; then
+if [ -n "${DOCKERHUB_REPOSITORY:-}" ]; then
+  DOCKER_REGISTRY_URL=${DOCKERHUB_REPOSITORY}
+
   registry:login() {
     $(docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD})
   }
