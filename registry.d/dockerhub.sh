0e69c435f6961577913cde33b846aee75ef7e399
Joakim Olsson <joakim@unbound.se>
2018-12-05 15:46:58 +0100

    Remove $() from dockerhub

--- registry.d/dockerhub.sh
+++ registry.d/dockerhub.sh
@@ -4,7 +4,7 @@ if [ -n "${DOCKERHUB_REPOSITORY:-}" ]; then
   DOCKER_REGISTRY_URL=${DOCKERHUB_REPOSITORY}
 
   registry:login() {
-    $(docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD})
+    docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD}
   }
 
   registry:create() {
