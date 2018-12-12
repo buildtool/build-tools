3f6c9da686b3a6f79322bb5b0cde8090bd74e928
Peter Svensson <peter@sparetimecoders.com>
2018-12-12 11:17:01 +0100

    Check for unbound variables earlier

--- scripts/registry.d/gitlab.sh
+++ scripts/registry.d/gitlab.sh
@@ -3,6 +3,9 @@
 if [ -n "${CI_REGISTRY_IMAGE:-}" ]; then
   DOCKER_REGISTRY_URL=${CI_REGISTRY_IMAGE%/*}
 
+  : ${CI_BUILD_TOKEN:?"CI_BUILD_TOKEN must be set"}
+  : ${CI_REGISTRY:?"CI_REGISTRY must be set"}
+
   registry:login() {
     docker login -u gitlab-ci-token -p ${CI_BUILD_TOKEN} ${CI_REGISTRY}
   }
@@ -14,5 +17,7 @@ fi
 
 if [ "${REGISTRY:-}" == "gitlab" ]; then
   echo "Will use Gitlab as container registry"
+  : ${GITLAB_GROUP:?"GITLAB_GROUP must be set"}
+
   DOCKER_REGISTRY_URL="registry.gitlab.com/$GITLAB_GROUP"
 fi
--- scripts/registry.d/quay.sh
+++ scripts/registry.d/quay.sh
@@ -2,6 +2,8 @@
 
 if [ -n "${QUAY_REPOSITORY:-}" ]; then
   DOCKER_REGISTRY_URL=${QUAY_REPOSITORY}
+  : ${QUAY_USERNAME:?"QUAY_USERNAME must be set"}
+  : ${QUAY_PASSWORD:?"QUAY_PASSWORD must be set"}
 
   registry:login() {
     docker login -u ${QUAY_USERNAME} -p ${QUAY_PASSWORD}
