8b264e43c702cd6288d78c0257b9b1cc100e1ed6
Peter Svensson <peter@sparetimecoders.com>
2019-09-25 09:59:42 +0200

    Removed env dump

--- cmd/build/build.go
+++ cmd/build/build.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"gitlab.com/sparetimecoders/build-tools/pkg/build"
 	"os"
-	"strings"
 )
 
 func main() {
 	dir, _ := os.Getwd()
 
-	fmt.Println(strings.Join(os.Environ(), "\n"))
 	exitFunc(build.DoBuild(dir, os.Stdout, os.Stderr, os.Args[1:]...))
 }
 
