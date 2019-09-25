c24de8807fa4f1ac39b5eac5e6d02af0cfb966d3
Peter Svensson <peter@sparetimecoders.com>
2019-09-25 08:46:08 +0200

    Dump env for testing

--- cmd/build/build.go
+++ cmd/build/build.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"gitlab.com/sparetimecoders/build-tools/pkg/build"
 	"os"
+	"strings"
 )
 
 func main() {
 	dir, _ := os.Getwd()
 
+	fmt.Println(strings.Join(os.Environ(), "\n"))
 	exitFunc(build.DoBuild(dir, os.Stdout, os.Stderr, os.Args[1:]...))
 }
 
