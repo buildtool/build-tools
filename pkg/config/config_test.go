2760e68ecfec8a0b9a3407db3c584fe1e2128bc7
Peter Svensson <peter@sparetimecoders.com>
2019-10-25 15:15:14 +0200

    feat: validate config

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/sparetimecoders/build-tools/pkg"
+	"github.com/sparetimecoders/build-tools/pkg/ci"
 	"github.com/sparetimecoders/build-tools/pkg/registry"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
@@ -56,7 +57,7 @@ func TestLoad_Empty(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, cfg)
 	assert.NotNil(t, cfg.CI)
-	assert.Equal(t, "", cfg.Scaffold.CI.Selected)
+	assert.Equal(t, ci.No{}.Name(), cfg.CurrentCI().Name())
 	assert.NotNil(t, cfg.Registry)
 	assert.Equal(t, "", cfg.Scaffold.RegistryUrl)
 	assert.Equal(t, "", out.String())
@@ -74,7 +75,7 @@ func TestLoad_BrokenYAML(t *testing.T) {
 	assert.EqualError(t, err, "yaml: unmarshal errors:\n  line 1: cannot unmarshal !!seq into config.CIConfig")
 	assert.NotNil(t, cfg)
 	assert.NotNil(t, cfg.CI)
-	assert.Equal(t, "", cfg.Scaffold.CI.Selected)
+	assert.Equal(t, ci.No{}.Name(), cfg.CurrentCI().Name())
 	assert.NotNil(t, cfg.Registry)
 	assert.Equal(t, "", cfg.Scaffold.RegistryUrl)
 	assert.Equal(t, fmt.Sprintf("\x1b[0mParsing config from file: \x1b[32m'%s/.buildtools.yaml'\x1b[39m\x1b[0m\n", name), out.String())
@@ -91,7 +92,7 @@ func TestLoad_UnreadableFile(t *testing.T) {
 	assert.EqualError(t, err, fmt.Sprintf("read %s: is a directory", filename))
 	assert.NotNil(t, cfg)
 	assert.NotNil(t, cfg.CI)
-	assert.Equal(t, "", cfg.Scaffold.CI.Selected)
+	assert.Equal(t, ci.No{}.Name(), cfg.CurrentCI().Name())
 	assert.NotNil(t, cfg.Registry)
 	assert.Equal(t, "", cfg.Scaffold.RegistryUrl)
 	assert.Equal(t, fmt.Sprintf("\x1b[0mParsing config from file: \x1b[32m'%s/.buildtools.yaml'\x1b[39m\x1b[0m\n", name), out.String())
@@ -102,26 +103,11 @@ func TestLoad_YAML(t *testing.T) {
 	defer func() { _ = os.RemoveAll(name) }()
 	yaml := `
 scaffold:
-  vcs:
-    selected: gitlab
-  ci:
-    selected: gitlab
   registry: quay.io
 registry:
-  dockerhub:
-    repository: repo
-    username: user
-    password: pass
   ecr:
     url: 1234.ecr
     region: eu-west-1
-  gitlab:
-    repository: registry.gitlab.com/group/project
-    token: token-value
-  quay:
-    repository: repo
-    username: user
-    password: pass
 environments:
   local:
     context: docker-desktop
@@ -136,13 +122,12 @@ environments:
 	assert.NoError(t, err)
 	assert.NotNil(t, cfg)
 	assert.NotNil(t, cfg.CI)
-	assert.Equal(t, "gitlab", cfg.Scaffold.CI.Selected)
+	assert.Equal(t, ci.No{}.Name(), cfg.CurrentCI().Name())
+
 	assert.NotNil(t, cfg.Registry)
 	assert.Equal(t, "quay.io", cfg.Scaffold.RegistryUrl)
-	assert.Equal(t, &registry.Dockerhub{Repository: "repo", Username: "user", Password: "pass"}, cfg.Registry.Dockerhub)
-	assert.Equal(t, &registry.ECR{Url: "1234.ecr", Region: "eu-west-1"}, cfg.Registry.ECR)
-	assert.Equal(t, &registry.Gitlab{Repository: "registry.gitlab.com/group/project", Token: "token-value"}, cfg.Registry.Gitlab)
-	assert.Equal(t, &registry.Quay{Repository: "repo", Username: "user", Password: "pass"}, cfg.Registry.Quay)
+	assert.Equal(t, "eu-west-1", cfg.CurrentRegistry().(*registry.ECR).Region)
+	assert.Equal(t, "1234.ecr", cfg.CurrentRegistry().(*registry.ECR).Url)
 	assert.Equal(t, 2, len(cfg.Environments))
 	assert.Equal(t, Environment{Context: "docker-desktop"}, cfg.Environments["local"])
 	devEnv := Environment{Context: "docker-desktop", Namespace: "dev"}
@@ -168,7 +153,7 @@ func TestLoad_BrokenYAML_From_Env(t *testing.T) {
 	assert.EqualError(t, err, "yaml: unmarshal errors:\n  line 1: cannot unmarshal !!seq into config.CIConfig")
 	assert.NotNil(t, cfg)
 	assert.NotNil(t, cfg.CI)
-	assert.Equal(t, "", cfg.Scaffold.CI.Selected)
+	assert.Equal(t, ci.No{}.Name(), cfg.CurrentCI().Name())
 	assert.NotNil(t, cfg.Registry)
 	assert.Equal(t, "", cfg.Scaffold.RegistryUrl)
 	assert.Equal(t, "Parsing config from env: BUILDTOOLS_CONTENT\n", out.String())
@@ -210,7 +195,6 @@ func TestLoad_YAML_DirStructure(t *testing.T) {
 	defer func() { _ = os.RemoveAll(name) }()
 	yaml := `scaffold:
   ci:
-    selected: gitlab
   registry: quay.io
 registry:
   dockerhub:
@@ -226,7 +210,6 @@ environments:
 	_ = os.Mkdir(filepath.Join(name, subdir), 0777)
 	yaml2 := `scaffold:
   ci:
-    selected: buildkite
 environments:
   test:
     context: ghi
@@ -238,7 +221,7 @@ environments:
 	assert.NoError(t, err)
 	assert.NotNil(t, cfg)
 	assert.NotNil(t, cfg.CI)
-	assert.Equal(t, "buildkite", cfg.Scaffold.CI.Selected)
+	assert.Equal(t, ci.No{}.Name(), cfg.CurrentCI().Name())
 	assert.NotNil(t, cfg.Registry)
 	assert.Equal(t, "quay.io", cfg.Scaffold.RegistryUrl)
 	currentRegistry := cfg.CurrentRegistry()
@@ -247,44 +230,4 @@ environments:
 	assert.Equal(t, "ghi", cfg.Environments["test"].Context)
 	assert.Equal(t, "def", cfg.Environments["local"].Context)
 	assert.Equal(t, fmt.Sprintf("\x1b[0mParsing config from file: \x1b[32m'%s/sub/.buildtools.yaml'\x1b[39m\x1b[0m\n\x1b[0mMerging with config from file: \x1b[32m'%s/.buildtools.yaml'\x1b[39m\x1b[0m\n", name, name), out.String())
-}
-
-func TestLoad_ENV(t *testing.T) {
-	defer pkg.SetEnv("CI", "gitlab")()
-	defer pkg.SetEnv("REGISTRY", "quay")()
-	out := &bytes.Buffer{}
-	cfg, err := Load(name, out)
-	assert.NoError(t, err)
-	assert.NotNil(t, cfg)
-	assert.NotNil(t, cfg.CI)
-	assert.Equal(t, "gitlab", cfg.Scaffold.CI.Selected)
-	assert.NotNil(t, cfg.Registry)
-	assert.Equal(t, "quay", cfg.Scaffold.RegistryUrl)
-	assert.Equal(t, "", out.String())
-}
-
-func TestLoad_Selected_VCS(t *testing.T) {
-	defer pkg.SetEnv("CI", "buildkite")()
-	defer pkg.SetEnv("VCS", "github")()
-	defer pkg.SetEnv("REGISTRY", "dockerhub")()
-	out := &bytes.Buffer{}
-	cfg, err := Load(name, out)
-	assert.NoError(t, err)
-	assert.NotNil(t, cfg)
-	assert.NotNil(t, cfg.VCS)
-	assert.Equal(t, "github", cfg.Scaffold.VCS.Selected)
-	assert.Equal(t, "", out.String())
-}
-
-func TestLoad_Scaffold_RegistryUrl(t *testing.T) {
-	defer pkg.SetEnv("CI", "gitlab")()
-	defer pkg.SetEnv("REGISTRY", "dockerhub")()
-	out := &bytes.Buffer{}
-	cfg, err := Load(name, out)
-	assert.NoError(t, err)
-	assert.NotNil(t, cfg)
-	assert.NotNil(t, cfg.CI)
-	assert.Equal(t, "gitlab", cfg.Scaffold.CI.Selected)
-	assert.Equal(t, "dockerhub", cfg.Scaffold.RegistryUrl)
-	assert.Equal(t, "", out.String())
 }
