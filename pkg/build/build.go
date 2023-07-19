1fd1ace6059ccb8a8561a14ef2274f43700c0208
Joakim Olsson <joakim@unbound.se>
2023-07-19 12:10:20 +0200

    fix: compile error

--- pkg/build/build.go
+++ pkg/build/build.go
@@ -295,7 +295,7 @@ func displayStatus(out *os.File, displayCh chan *client.SolveStatus, eg *errgrou
 	}
 	// not using shared context to not disrupt display but let it finish reporting errors
 	eg.Go(func() error {
-		_, err := progressui.DisplaySolveStatus(context.TODO(), "", c, out, displayCh)
+		_, err := progressui.DisplaySolveStatus(context.TODO(), c, out, displayCh)
 		return err
 	})
 }
