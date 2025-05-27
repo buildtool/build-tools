19e9784368c70c7e2b3d92dd47db8089ffd25cc1
Peter Svensson <peter@sparetimecoders.com>
2025-05-27 12:34:38 +0200

    chore: clean up configuration files and enhance hooks
    Remove the unused commitlint configuration and update pre-commit 
    hooks by adding additional checks for executables and scripts. 
    Update the golang pre-commit hook references for better 
    compatibility. These changes streamline the commit process and 
    improve code quality checks.

--- .github/commitlint.config.mjs
+++
@@ -1,9 +0,0 @@
-export default {
-  extends: ['@commitlint/config-conventional'],
-  /*
-   * Any rules defined here will override rules from @commitlint/config-conventional
-   */
-  rules: {
-    'body-max-line-length': [2, 'always', 200],
-  },
-};
